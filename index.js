be3c583f9deb96f04362b3a4be845bd2d072e03e
Nikita-Babenko <bobkage3@gmail.com>
2017-09-13 18:26:07 +0300

    1

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+
+const types = {
+    '.js':{
+        contentType:'text/javascript'
+    },
+    '.css':{
+        contentType:'text/css'
+    },
+    '.jpg':{
+        contentType:'text/jpg'
+    },
+    '.html':{
+        contentType:'text/html'
+    },
+    '.mp3':{
+        contentType:'audio/mpeg'
+    },
+    'default':{
+        contentType:'text/plain'
+    },
+
+};
 
 const server= http.createServer();
+
 server.on('request',(request,response)=>{
+
     const {method, url, headers} = request;
+
     console.log(`${method} ${url}`);
 
-        response.setHeader("Content-Type","text/html");
+    let filePath = url;
 
-    let filePath = '/projects/NoosphereNodeJS/kub/index.html';
+    if (filePath === '/'){
+        filePath = '/index.html';
+    }
+
+    let fileExt = path.extname(filePath);
+
+    let responseParams = types[fileExt] || types.default;
+
+    response.setHeader("Content-Type",responseParams.contentType);
+
+     filePath = '/projects/NoosphereNodeJS/kub/'+ filePath;
     let readStream = fs.createReadStream(filePath);
 
+    readStream.on('error', err => {
+        console.log(err);
+
+        response.statusCode = 404;
+        response.end();
+    });
 
     readStream.pipe(response);
 
